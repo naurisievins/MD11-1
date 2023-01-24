eb19b919b6b9964816a19e097ea629bab0f5fa11
naurisievins <nauris.ievins@gmail.com>
2023-01-24 18:20:22 +0200

    Country table with MetroUI

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,1 +1,30 @@
 import axios from 'axios';
+
+// For editing JSON file required for metroui
+const toEditJSON = () => {
+  const countriesFiltered: ((string | number)[])[] = [];
+  // const html:HTMLDivElement = document.querySelector(".arr");
+  const filterCountry = (apiResponse:any) => {
+    let id = 1;
+    for (const country of apiResponse) {
+      let countryArr: (string | number)[] = [];
+      countryArr.push(id);
+      countryArr.push(country.name);
+      countryArr.push(country.capital);
+      countryArr.push(country.currency.name);
+      countryArr.push(country.language.name);
+      countriesFiltered.push(countryArr);
+      // html.innerText += JSON.stringify(countryArr) + ",\n";
+      id += 1;
+      countryArr = [];
+    }
+  };
+
+  const api = 'http://localhost:3004/countries';
+
+  axios.get(api)
+    .then((response) => filterCountry(response.data))
+    .catch((error) => {
+      console.log(error);
+    });
+};
